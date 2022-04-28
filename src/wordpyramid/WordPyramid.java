0e23553f48ff5677e29463e2d3c8008be8f32f4e
BNKDuy <100258881+BNKDuy@users.noreply.github.com>
2022-04-27 23:22:21 -0400

    April 27

--- src/wordpyramid/WordPyramid.java
+++ src/wordpyramid/WordPyramid.java
@@ -6,10 +6,7 @@ public class WordPyramid {
 
     public static void Pyramid(String in)
     {
-        if (in.length() == 1)
-        {
-            System.out.println(in);
-        }
+        if (in.length() == 1) System.out.println(in);
         else if (in.length() != 1 && in.length() != 0)
         {
             System.out.println(in);
@@ -17,14 +14,15 @@ public class WordPyramid {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
         String in;
 
         System.out.println("Enter your word:");
         in = input.nextLine();
 
+        System.out.println();
         Pyramid(in);
     }
-    
 }
